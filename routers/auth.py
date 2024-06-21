cfeadd4fd1cb6175da5f16c1ba10dc2ebc2223b2
Jose Michael Rubio <contact@josemichaelrubio.com>
2024-06-21 13:55:06 -0400

    Some hashing was added

--- routers/auth.py
+++ routers/auth.py
@@ -1,9 +1,14 @@
+from typing import Annotated
 from fastapi import APIRouter, Depends, HTTPException, status
 
+from passlib.context import CryptContext
 from pydantic import BaseModel
+from database import SessionLocal
 from models import Users
 
 router = APIRouter()
+db_dependency = Annotated[SessionLocal, Depends(get_db)]
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 class CreateUserRequest(BaseModel):
     id: int
@@ -17,9 +22,17 @@ async def get_user():
 
 @router.post("/auth")
 async def create_user(user: CreateUserRequest):
+    hashed_password = bcrypt_context.hash(user.password)
     create_user_model = Users(
         id=user.id, 
         username=user.username, 
         password=user.password, 
         email=user.email)
     return create_user_model
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
